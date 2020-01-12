409712d7498474e8b2d4a082922d9caa21303948
Nicholas Tran <phikhant@uci.edu>
2020-01-11 20:10:36 -0800

    Add registration form

--- users/views.py
+++ users/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .forms import UserRegisterForm
 
-# Create your views here.
+def login(request):
+    return render(request, 'users/login.html')
+
+def register(request):
+    if request.method == "POST":
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'MakeBank account created for {username}!')
+            return redirect('users-login')
+    else:
+        form = UserRegisterForm()
+    context = {'form': form, 'title': "Sign Up"}
+    return render(request, 'users/register.html', context)
